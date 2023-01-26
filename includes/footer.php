dbd0a8792f6195ea11ffae79035c80ffba33fa70
David Padilla <davidpadilla30oc@gmail.com>
2023-01-26 17:31:13 -0500

    Agregamos ajax para la pagina no recargable

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,34 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+<script>
+  function ajax(enlace){
+    const http = new XMLHttpRequest();
+    const url = enlace;
+
+    http.onreadystatechange = function(){
+      if (this.readyState == 4 && this.status == 200) {
+          console.log(this.responseText);
+          document.getElementById("response").innerHTML = this.responseText;
+      }
+    }
+
+    http.open("GET", url);
+    http.send();
+
+  }
+  document.getElementById("botonphp").addEventListener("click", function(){
+    ajax('http://localhost/cursos/login.php');
+  });
+  document.getElementById("botongit").addEventListener("click", function(){
+    ajax('http://localhost/cursos/login.php');
+  });
+  document.getElementById("botonmark").addEventListener("click", function(){
+    ajax('http://localhost/cursos/login.php');
+  });
+  document.getElementById("botonpy").addEventListener("click", function(){
+    ajax('http://localhost/cursos/login.php');
+  });
+</script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
   </body>
--- index.php
+++ index.php
@@ -5,14 +5,14 @@ include "includes/header.php";
     <div class="row">
         <div class="col-lg-5">
             <div class="list-group">
-                <button class="btn btn-primary rounded-0 border-3 border-light text-start mb-1"><i class="fab fa-php"></i> PHP</button>
-                <button class="btn btn-primary rounded-0 border-3 border-light text-start mb-1"><i class="fab fa-git"></i> <i class="fab fa-github"></i> git y github</button>
-                <button class="btn btn-primary rounded-0 border-3 border-light text-start mb-1"><i class="fab fa-markdown"></i> markdown</button>
-                <button class="btn btn-primary rounded-0 border-3 border-light text-start mb-1"><i class="fab fa-python"></i> python</button>
+                <button id="botonphp" class="btn btn-primary rounded-0 border-3 border-light text-start mb-1"><i class="fab fa-php"></i> PHP</button>
+                <button id="botongit" class="btn btn-primary rounded-0 border-3 border-light text-start mb-1"><i class="fab fa-git"></i> <i class="fab fa-github"></i> git y github</button>
+                <button id="botonmark" class="btn btn-primary rounded-0 border-3 border-light text-start mb-1"><i class="fab fa-markdown"></i> markdown</button>
+                <button id="botonpy" class="btn btn-primary rounded-0 border-3 border-light text-start mb-1"><i class="fab fa-python"></i> python</button>
             </div>
         </div>
         <div class="col-lg-7">
-            <code>#gasd</code>
+            <code id="response">#gasd</code>
         </div>
     </div>
 <?php
